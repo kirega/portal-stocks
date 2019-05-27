58cd9a7548afbb8ae807a0fa451ffffe87f7cc69
kirega <mtotomdogo96>
2019-05-28 01:00:08 +0300

    [finishes #166266837] CRUD functionality for products

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,7 +5,7 @@ class MainNavbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Stocks</a>
+        <a className="navbar-brand" href="#">Prox Stocks</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
                 aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"/>
@@ -13,17 +13,17 @@ class MainNavbar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <span className="nav-link"> <Link to={'/inventory'}>Inventory</Link> </span>
-            </li>
+            {/*<li className="nav-item active">*/}
+            {/*  <span className="nav-link"> <Link to={'/inventory'}>Inventory</Link> </span>*/}
+            {/*</li>*/}
             <li className="nav-item">
               <span className='nav-link'><Link to={'/products'}>Products</Link></span>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          {/*<form className="form-inline my-2 my-lg-0">*/}
+          {/*  <input className="form-control mr-sm-2" type="search" placeholder="Search"/>*/}
+          {/*  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>*/}
+          {/*</form>*/}
         </div>
       </nav>
   );};
--- src/components/product.component.js
+++ src/components/product.component.js
@@ -2,30 +2,45 @@ import React, { PureComponent } from "react";
 import MainNavbar from "./navbar.component";
 import axios from 'axios';
 
-const SELECTION  = ['KG','UNIT', 'PKT'];
+const SELECTION = ['KG', 'UNIT', 'PKT'];
+var product = {};
 
 class Product extends PureComponent {
   constructor(props) {
     super(props);
+    this.getProducts = this.getProducts.bind(this);
+    this.deleteProduct = this.deleteProduct.bind(this);
+    this.createProduct = this.createProduct.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePriceChange = this.handlePriceChange.bind(this);
+    this.handleUomChange = this.handleUomChange.bind(this);
+    this.editProduct = this.editProduct.bind(this);
+    this.addProduct =  this.addProduct.bind(this);
+    this.updateProduct = this.updateProduct.bind(this);
 
     this.state = {
-      products: [
-        {
-          'name': '',
-          'uom': '',
-          'price': 0
-        }
-      ],
+      products: [],
       show: false,
+      initialForm: {
+        id:'',
+        name: '',
+        uom: '',
+        price: '',
+      },
+      add: false,
+      edit: false,
     };
   }
 
   componentDidMount() {
-    axios.get('http://prox-stock.herokuapp.com:8000/products/')
+    this.getProducts();
+  }
+
+  getProducts() {
+    axios.get('http://localhost:8000/products/')
       .then(
         res => {
-          this.setState( prevState => {
-            console.log(res.data);
+          this.setState(prevState => {
             return prevState.products = res.data
           })
         }
@@ -33,65 +48,222 @@ class Product extends PureComponent {
       .catch(err => {
         console.log(err)
       })
+  };
+
+  createProduct(e) {
+    e.preventDefault();
+    const { initialForm } = this.state;
+    if (Object.keys(initialForm).length > 0) {
+      console.log(initialForm);
+      axios.post(`http://localhost:8000/products/`, initialForm).then(
+        (res) => {
+          this.getProducts();
+        }
+      )
+    } else {
+      return;
+    }
   }
 
+  deleteProduct(id) {
+    axios.delete(`http://localhost:8000/products/${ id }`).then(
+      () => {
+        this.getProducts();
+      }
+    );
+  };
+  updateProduct(e) {
+    e.preventDefault();
+    const {initialForm} = this.state;
+    axios.put(`http://localhost:8000/products/${ initialForm.id }`, initialForm).then(
+      () => {
+        this.getProducts();
+      }
+    );
+  }
+  editProduct(product) {
+    const { edit }= this.state;
+    this.setState({edit: !edit, initialForm:product});
+  }
+  addProduct (){
+    const { add }= this.state;
+    this.setState({add: !add});
+  }
+
+  handleNameChange(e) {
+    let value = e.target.value;
+    this.setState(prevState => ({
+        initialForm:
+          { ...prevState.initialForm, name: value }
+      })
+    )
+  }
+
+  handlePriceChange(e) {
+    let value = e.target.value;
+    this.setState(prevState => ({
+        initialForm:
+          { ...prevState.initialForm, price: value }
+      })
+    )
+  }
+
+  handleUomChange(e) {
+    let value = e.target.value;
+    this.setState(prevState => ({
+        initialForm:
+          { ...prevState.initialForm, uom: value }
+      })
+    )
+  };
+
   render() {
+    const { initialForm, add, edit } = this.state;
     return (
       <div>
         <MainNavbar/>
-          <h3>Products</h3>
-            <form className="col-6 mx-auto">
-              <div className="form-group">
-                <h4>Add new product</h4>
-              </div>
-              <div className="form-group">
-                <label>Name</label>
-                <input type="text" className="form-control" aria-describedby="name" placeholder="Enter product name"/>
-              </div>
-              <div className="form-group">
-                <label>Price</label>
-                <input type="number" min="0.01" step="0.01" className="form-control" placeholder="price"/>
-              </div>
-              <div className="form-group">
-                <label>Unit of Measure</label>
-                <select className="form-control">
-                  <option>-</option>
-                  { SELECTION.map((value, index) => {
-                    return (<option key={ index }>{ value }</option>);
-                  }) }
-                </select>
-              </div>
-              <div className="form-row">
-                <div className="form-group col-6">
-                  <button type="submit" className="btn btn-primary col">Submit</button>
+        <div className="container">
+          <div className="row justify-content-around align-content-end mt-2">
+            <div className="col-6">
+              <h3>Products</h3>
+            </div>
+            <div className="col-2">
+              <button className="btn btn-primary" onClick={this.addProduct}>{add ? "Hide": "Add Product" }</button>
+            </div>
+          </div>
+          {
+            add ?
+              <form className="col-6 mx-auto" onSubmit={ this.createProduct }>
+                <div className="form-group">
+                  <h4>Add new product</h4>
                 </div>
-                <div className="form-group col-6">
-                  <button type="button" className="btn btn-danger col">Close</button>
+                <div className="form-group">
+                  <label>Name</label>
+                  <input type="text"
+                         className="form-control"
+                         name="name"
+                         value={ initialForm.name }
+                         aria-describedby="name"
+                         required
+                         onChange={ this.handleNameChange }
+                         placeholder="Enter product name"/>
                 </div>
-              </div>
-            </form>
-          <table className="table table-dark">
-            <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">UoM</th>
-              <th scope={ 'col' }> Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            { this.state.products.map((value, index) => {
-              return (
-                <tr key={ index }>
-                  <th>{ index }</th>
-                  <td>{ value.name }</td>
-                  <td>{ value.uom }</td>
-                  <td>{ value.price }</td>
-                </tr>);
-            }) }
-            </tbody>
-          </table>
+                <div className="form-group">
+                  <label>Price</label>
+                  <input type="number"
+                         value={ initialForm.price }
+                         required min="0.01"
+                         step="0.01"
+                         name="price"
+                         onChange={ this.handlePriceChange }
+                         className="form-control"
+                         placeholder="price"/>
+                </div>
+                <div className="form-group">
+                  <label>Unit of Measure</label>
+                  <select name="uom" className="form-control" value={ initialForm.uom }
+                          onChange={ this.handleUomChange }
+                          required>
+                    <option>-</option>
+                    { SELECTION.map((value, index) => {
+                      return (<option key={ index }>{ value }</option>);
+                    }) }
+                  </select>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-6">
+                    <button type="submit" className="btn btn-primary col">Submit</button>
+                  </div>
+                  <div className="form-group col-6">
+                    <button type="button" className="btn btn-danger col" onClick={this.addProduct}>Close</button>
+                  </div>
+                </div>
+              </form>
+              : ''
+          }
+
+          { this.state.products.length === 0 ?
+            <div> No products exist Yet </div> :
+            <table className="table table-dark">
+              <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">UoM</th>
+                <th scope={ 'col' }> Price</th>
+                <th scope={ 'col' }>Action</th>
+              </tr>
+              </thead>
+              <tbody>
+              { this.state.products.map((value, index) => {
+                return (
+                  <tr key={ index + 1 }>
+                    <th>{ index + 1 }</th>
+                    <td>{ value.name }</td>
+                    <td>{ value.uom }</td>
+                    <td>{ value.price }</td>
+                    <td>
+                      <button className="btn btn-primary mr-2" onClick={ () => this.editProduct(value) }> Edit
+                      </button>
+                      <button className="btn btn-danger" onClick={ () => this.deleteProduct(value.id) }>delete</button>
+                    </td>
+                  </tr>);
+              }) }
+              </tbody>
+            </table>
+          }
+          {
+            edit ?
+              <form className="col-6 mx-auto" onSubmit={this.updateProduct}>
+                <div className="form-group">
+                  <h4>Edit product</h4>
+                </div>
+                <div className="form-group">
+                  <label>Name</label>
+                  <input type="text"
+                         className="form-control"
+                         name="name"
+                         value={ initialForm.name }
+                         aria-describedby="name"
+                         required
+                         onChange={ this.handleNameChange }
+                         placeholder="Enter product name"/>
+                </div>
+                <div className="form-group">
+                  <label>Price</label>
+                  <input type="number"
+                         value={ initialForm.price }
+                         required min="0.01"
+                         step="0.01"
+                         name="price"
+                         onChange={ this.handlePriceChange }
+                         className="form-control"
+                         placeholder="price"/>
+                </div>
+                <div className="form-group">
+                  <label>Unit of Measure</label>
+                  <select name="uom" className="form-control" value={ initialForm.uom }
+                          onChange={ this.handleUomChange }
+                          required>
+                    <option>-</option>
+                    { SELECTION.map((value, index) => {
+                      return (<option key={ index }>{ value }</option>);
+                    }) }
+                  </select>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-6">
+                    <button type="submit" className="btn btn-primary col" >Update</button>
+                  </div>
+                  <div className="form-group col-6">
+                    <button type="button" className="btn btn-danger col" onClick={this.editProduct}>Close</button>
+                  </div>
+                </div>
+              </form>
+              : ''
+          }
         </div>
+      </div>
     );
   };
 }
