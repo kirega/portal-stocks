4fa09a3977686a390b0cda0fb288b86dca48f6d1
kirega <mtotomdogo96>
2019-05-28 13:19:26 +0300

    [finishes #166266837] update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
 const app = express();
+const port = process.env.PORT || 8080;
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + 'dist/'));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// send the user to index html page inspite of the url
+// Static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// wildcard route
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'));
+  res.sendFile(path.join(`${__dirname}/dist/index.html
 });
-
-app.listen(port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: ['babel-loader'],
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader"
-            }
-          ]  
-      }
-    ]
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+      {
+        test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
+        use: {
+          loader: 'url-loader?limit=5000',
+        },
+      },
+    ],
   },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
+    contentBase: path.join(__dirname, 'dist'),
     historyApiFallback: true,
+    port: 8080,
+    compress: true,
+    hot: true,
   },
-  plugins: [htmlPlugin]
+  mode: 'development',
 };
