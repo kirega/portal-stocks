a6910d18d271b9a0002ddb9c197ec9d64363c5e1
kirega <mtotomdogo96>
2019-05-28 13:20:22 +0300

    [finishes #166266837] update server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // wildcard route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/index.html
+  res.sendFile(path.join(`${__dirname}/dist/index.html`))
 });
